90ac30fcaa15972563009a8b07f26ecb12cdd654
Max Tsero <max.tsero@outlook.com>
2020-08-16 12:05:24 +0100

    More updates.

--- commands/ping.js
+++ commands/ping.js
@@ -2,16 +2,19 @@
  * Copyright (c) Max Tsero. All rights reserved.
  * Licensed under the MIT License.
  */
+const Math = require('../utils/math')
 module.exports = {
   name: 'ping',
-  description: 'Asks the bot to return a confirmation that it\'s online.',
+  description: 'Asks the bot to return a confirmation that it\'s online along with it\'s message and API latency figures.',
   usage: '',
   cooldown: 10,
   /**
    * Provides a simple reply to confirm operation.
    * @return {null}
    */
-  run (client, message, args) {
-    message.channel.send('pong!').catch(console.error)
+  async run (client, message, args) {
+    const msg = await message.channel.send("Ping?");
+    console.log('Client:', client)
+    msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
   }
 }
